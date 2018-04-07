a2ea063b7d466a87d0d436c35d8ec51445e211cc
Jinshuo Bai <baijinshuo_1209@hotmail.com>
2018-04-07 17:32:11 -0400

    Josephus Problem

---
+++ Josephus.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+using namespace std;
+typedef struct _LinkNode {
+	int value;
+	struct _LinkNode* next;
+} LinkNode, *LinkNodePtr;
+
+LinkNodePtr createCycle(int total) {
+	int index = 1;
+	LinkNodePtr head = NULL, curr = NULL, prev = NULL;
+	head = (LinkNodePtr)malloc(sizeof(LinkNode));
+	head->value = index;
+	prev = head;
+
+	while (--total > 0) {
+		curr = (LinkNodePtr)malloc(sizeof(LinkNode));
+		curr->value = ++index;
+		prev->next = curr;
+		prev = curr;
+	}
+	curr->next = head;
+	return head;
+}
+
+void run(int total, int tag) {
+	LinkNodePtr node = createCycle(total);
+	LinkNodePtr prev = NULL;
+	int start = 1;
+	int index = start;
+	while (node && node->next) {
+		if (index == tag) {
+			printf("\n%d", node->value);
+			if (tag == start) {
+				prev = node->next;
+				node->next = NULL;
+				node = prev;
+			}
+			else {
+				prev->next = node->next;
+				node->next = NULL;
+				node = prev->next;
+			}
+			index = start;
+		}
+		else {
+			prev = node;
+			node = node->next;
+			index++;
+		}
+	}
+}
+int main() {
+	run(5, 999999);
+	return 0;
+}
